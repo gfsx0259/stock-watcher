b88072539824fcf69fb0f4b517c62ec7702401a9
Konstantin Popov <k.popov@it.ecommpay.com>
2020-05-11 16:16:31 +0300

    Fetch data from Moex ISS API

--- decode.go
+++ decode.go
@@ -1,45 +1,64 @@
 package main
 
-import(
+import (
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 type Row struct {
-	BuyPrice float32 `xml:"BID,attr"`
+	BuyPrice  float32 `xml:"BID,attr"`
 	SalePrice float32 `xml:"OFFER,attr"`
-	Ticker string `xml:"SECID,attr"`
+	Ticker    string  `xml:"SECID,attr"`
 }
 
-type Rows struct {
-	Row []Row `xml:"row"`
+func (row Row) String() string {
+	return fmt.Sprintf("{buyPrice: %f, salePrice: %f, ticker: %s}\n", row.BuyPrice, row.SalePrice, row.Ticker)
 }
 
 type Data struct {
-	Rows Rows `xml:"rows"`
+	Rows []Row  `xml:"rows>row"`
+	Id   string `xml:"id,attr"`
 }
-
 type Document struct {
 	Data []Data `xml:"data"`
 }
 
-func main() {
-	var err error
+const ApiEndpoint = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.xml"
 
-	data, err := ioutil.ReadFile("decode.xml")
+func main() {
+	response, err := http.Get(ApiEndpoint)
 	if err != nil {
-		fmt.Println("File reading error", err)
+		fmt.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	emptyDoc := Document{}
-
-	err = xml.Unmarshal(data, &emptyDoc)
+	doc := Document{}
+	err = xml.Unmarshal(body, &doc)
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		return
 	}
 
-	fmt.Println(emptyDoc.Data)
+	var stocks []Row
+	for _, data := range doc.Data {
+		if data.Id == "marketdata" {
+			stocks = data.Rows
+		}
+	}
+
+	indexedStocks := make(map[string]Row)
+
+	for _, row := range stocks {
+		indexedStocks[row.Ticker] = row
+	}
+	fmt.Println(indexedStocks)
 }
